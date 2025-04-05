95782e1abd4227df0cf2178b27692adc707ceb99
ImaginedTime <udayomsrivastava@kgpian.iitkgp.ac.in>
2025-04-06 03:24:23 +0530

    timeout

--- main.py
+++ main.py
@@ -6,7 +6,5 @@ if __name__ == "__main__":
         host="127.0.0.1",
         port=8080,
         reload=True,
-        timeout_keep_alive=86400, # 1 day in seconds
-        timeout_notify=0,  # Disable timeout notifications
-        timeout_graceful_shutdown=None  # No graceful shutdown timeout
+        timeout_keep_alive=86400,
     )
